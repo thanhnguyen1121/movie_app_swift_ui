ec37396a8af0eb796c6bee98ef6914ab78ddc34b
thanhnguyen <thanhnguyen9908@gmail.com>
2023-01-15 15:31:43 +0700

    Initial Commit

---
+++ demo/demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  demoApp.swift
+//  demo
+//
+//  Created by Nguyen Thanh on 15/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
